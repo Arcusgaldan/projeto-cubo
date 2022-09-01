103ca2dfc89d698f4a152a176093ca6ede4e5496
Arcusgaldan <thales_de_mattos@hotmail.com>
2022-08-31 22:52:34 -0300

    Initial Commit

---
+++ src/routes/page1/index.jsx
@@ -0,0 +1,8 @@
+export default function Page1() {
+    return (
+      <div style={{ display: "flex", flex: 1, height: "100vh", justifyContent: "center", alignItems: "center"}}>
+        <div style={{border: 'solid', height: "40%", width: "30%"}}>          
+        </div>
+      </div>
+    );
+  }
---
+++ src/routes/page2/index.jsx
@@ -0,0 +1,8 @@
+export default function Page2() {
+    return (
+      <div style={{display: "flex", height: "100vh"}}>
+        <menu style={{height: "5%", width: "100%", border: 'solid'}}>menu</menu>
+        <footer style={{justifyContent: 'flex-end', height: "5%", width: "100%", border: 'solid'}}>footer</footer>
+      </div>
+    );
+  }
